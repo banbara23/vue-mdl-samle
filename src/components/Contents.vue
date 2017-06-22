a01b1d0a72669f5f284bba81652a561c340970c4
ikemura <banbara.work@gmail.com>
2017-06-22 14:57:33 +0900

    お試しコンテンツ追加

---
+++ src/components/Contents.vue
@@ -0,0 +1,177 @@
+<template>
+<pre>
+キーワードを入力
+募集QiitaやQiita:Teamを良くしたいエンジニア
+投稿するストック一覧
+0
+
+プログラミング知識を共有しよう。
+
+ノウハウ・Tipsを投稿する
+フィード
+すべての投稿
+自分の投稿 8
+ fujimura_i_kai
+fujimura_i_kai がいいねしました
+あっと驚かせるJavaプログラミング(をやめよう） 
+Java
+アンチパターン
+ 413    6
+
+ JavaScript
+JavaScript に新しい投稿がありました
+JavaScriptで、switch case でラベルをカンマで連結したらだめ 
+JavaScript
+ 0    2
+
+ nodejs
+nodejs に新しい投稿がありました
+Node.jsとNEM-sdkを利用して仮想通貨XEMのばらまきサイトを超簡単に作る。
+nodejs
+Bitcoin
+Blockchain
+NEM
+xem
+ 3    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+Velocity.js 一時停止・再生機能について 
+JavaScript
+jQuery
+アニメーション
+Velocity
+ 3    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+[checkio]Weak point 
+JavaScript
+ 1    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+２次元配列の設定 
+JavaScript
+ 0    0
+
+ スクレイピング
+スクレイピング に新しい投稿がありました
+C#でモダンにスクレイピングするならAngleSharp 
+HTML5
+C#
+スクレイピング
+scraping
+AngleSharp
+ 16    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+Javascriptの実行順序(Web Worker、非同期処理、イベント ) 
+JavaScript
+WebWorker
+ 4    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+Vector Tiles to start in Japan (using Govt. Data) 
+Ruby
+JavaScript
+ 0    0
+
+ Android
+Android に新しい投稿がありました
+深層学習の訓練用画像のラベルをスマホで作成 [紹介] 
+Android
+DeepLearning
+ 6    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+Marionette.jsとFluxを組み合わせてみる 
+JavaScript
+Backbone.js
+flux
+Marionette.js
+ 2    0
+
+ Android
+Android に新しい投稿がありました
+ここまでできる！はじめてのAI開発 Microsoft Cognitive Services編
+Android
+Microsoft
+iOS
+AI
+人工知能
+ 2    0
+
+ Android
+Android に新しい投稿がありました
+仮想マシンとAndroid実機によるReact Native入門というか触りだけ 
+Android
+VirtualBox
+reactnative
+ 0    0
+
+ Android
+Android に新しい投稿がありました
+英語でKotlinを勉強するためのリソース 
+Android
+Kotlin
+ 3    0
+
+ linebot
+linebot に新しい投稿がありました
+位置情報を元にHotelとBarが検索できるLINEBOTをリリースしました。
+Python
+Django
+Heroku
+Line
+linebot
+ 1    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+Vue.js用テストライブラリ「avoriaz」入門 
+JavaScript
+テスト
+vue.js
+avoriaz
+ 16    0
+
+ Node.js
+Node.js に新しい投稿がありました
+【メモ】nvm0.33.2 でnode.jsのインストールがこける場合の対処法 
+Node.js
+ 0    0
+
+ Android
+Android に新しい投稿がありました
+potatotips #41でAndroidのFont周りの新機能について発表しました。 
+Android
+ 2    0
+
+ JavaScript
+JavaScript に新しい投稿がありました
+HTAで高速にExcelデータを読み込む 
+JavaScript
+Excel
+hta
+ 0    3
+
+もっと見る
+
+banbara ikemura
+@banbara23
+8投稿12Contribution
+ フィード
+ Organization一覧
+ 人気の投稿 beta
+ Advent Calendar
+ 知り合いかも
+mnrck07
+k_anz__
+© 2011-2017 Increments Inc.利用規約プライバシーヘルプお問い合わせQiitaとはユーザータグブログAPITeamKobito ご意見 
+
+</pre>
+</template>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="hello">
-    <mdl-badge :badge="value">
-      <span>Inbox</span>
-    </mdl-badge>
+  <div class="demo-layout-waterfall mdl-layout mdl-js-layout">
+    <header class="mdl-layout__header mdl-layout__header--waterfall">
+      <!-- Top row, always visible -->
+      <div class="mdl-layout__header-row">
+        <!-- Title -->
+        <span class="mdl-layout-title">Title</span>
+        <!--<div class="mdl-layout-spacer"></div>-->
+        <!--<div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                        mdl-textfield--floating-label mdl-textfield--align-right">
+          <label class="mdl-button mdl-js-button mdl-button--icon"
+                 for="waterfall-exp">
+            <i class="material-icons">search</i>
+          </label>
+          <div class="mdl-textfield__expandable-holder">
+            <input class="mdl-textfield__input"
+                   type="text"
+                   name="sample"
+                   id="waterfall-exp">
+          </div>
+        </div>-->
+      </div>
+      <!-- Bottom row, not visible on scroll -->
+      <div class="mdl-layout__header-row">
+        <div class="mdl-layout-spacer"></div>
+        <!-- Navigation -->
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link"
+             href="">Link1</a>
+          <a class="mdl-navigation__link"
+             href="">Link2</a>
+          <a class="mdl-navigation__link"
+             href="">Link3</a>
+          <a class="mdl-navigation__link"
+             href="">Link4</a>
+        </nav>
+      </div>
+    </header>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">Title</span>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link"
+           href="">Link1</a>
+        <a class="mdl-navigation__link"
+           href="">Link2</a>
+        <a class="mdl-navigation__link"
+           href="">Link3</a>
+        <a class="mdl-navigation__link"
+           href="">Link4</a>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <!-- Your content goes here -->
+        <contents></contents>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
+import contents from '@/components/Contents'
 export default {
-  name: 'hello',
   data() {
     return {
       value: 3
     }
+  },
+  components: {
+    contents
   }
 }
 </script>
@@ -36,5 +91,9 @@ li {
 
 a {
   color: #42b983;
+}
+
+.demo-layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type {
+  padding-right: 0;
 }
 </style>
